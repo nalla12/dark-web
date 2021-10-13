7e4a55c61156a47bb8114a5280565dc742ce54ff
AD-SURFACE-PRO\Allan D <alland12@hotmail.com>
2021-10-14 00:47:45 +0200

    Created details pane component. Reordered imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
-import { FormsModule } from "@angular/forms";
 import { TerminalInputComponent } from './terminal/terminal-input/terminal-input.component';
+import { DetailsPaneComponent } from './details-pane/details-pane.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TerminalComponent,
-    TerminalInputComponent
+    TerminalInputComponent,
+    DetailsPaneComponent
   ],
     imports: [
         BrowserModule,
---
+++ src/app/details-pane/details-pane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-pane',
+  templateUrl: './details-pane.component.html',
+  styleUrls: ['./details-pane.component.css']
+})
+export class DetailsPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
