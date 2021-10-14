6322837ecc1e05260c4b9508968d24bd3f38c767
AD-SURFACE-PRO\Allan D <alland12@hotmail.com>
2021-10-14 15:21:14 +0200

    Rettelser

--- src/app/cmd.service.ts
+++ src/app/cmd.service.ts
@@ -13,18 +13,69 @@ export class CmdService {
       case 'om':
         this.history.push('Full-stack programmør med mere end 10 års erfaring.');
         break;
-      case 'language':
-      case 'languages':
-      case 'sprog':
-        this.history.push('PHP, Java, Swift, Objective-C, C#, TypeScript, JavaScript');
-        break;
-      case 'help':
-      case 'hjælp':
-        this.history.push('This is a list of possible commands <li>Command 1<li>Command 2 etc');
-        break;
       case 'contact':
       case 'kontakt':
         this.history.push('Email: example@leadcoding.dk | Tlf.: 88888888');
+        break;
+      case 'help':
+      case 'hjælp':
+        this.history.push(`Her er listen over mulige kommandoer:<ul>
+          <li>Command 1</li>
+          <li>Command 2</li>
+          <li>Command 3</li>
+        </ul>`);
+        break;
+      case 'language':
+      case 'languages':
+      case 'lang':
+      case 'sprog':
+        this.history.push(`<ul>
+          <li>PHP</li>
+          <li>Java</li>
+          <li>Swift</li>
+          <li>Objective-C</li>
+          <li>C#</li>
+          <li>TypeScript</li>
+          <li>JavaScript</li>
+        </ul>`);
+        break;
+      case 'service':
+      case 'services':
+      case 'product':
+      case 'products':
+      case 'produkter':
+        this.history.push(`<ul>
+          <li>Senior Backend Development</li>
+          <li>Software/System Architect</li>
+          <li>DevOps / DevSecOps</li>
+        </ul>`);
+        break;
+      case 'technologies':
+      case 'technology':
+      case 'tekonologi':
+      case 'tech stack':
+      case 'tech':
+        this.history.push(`<ul>
+          <li>Infrastructure<ul>
+            <li>Google Cloud Platform</li>
+            <li>Kubernetes</li>
+            <li>Micro Services</li>
+            <li>Docker</li>
+          </ul></li>
+          <li>Web Development<ul>
+            <li>PHP<ul>
+              <li>Code Igniter</li>
+              <li>Laravel</li>
+            </ul></li>
+            <li>Angular</li>
+            <li>Vue</li>
+          </ul></li>
+          <li>App Development<ul>
+            <li>Xamarin</li>
+            <li>Android / Java</li>
+            <li>iOS / Swift</li>
+          </ul></li>
+        </ul>`);
         break;
       case 'work':
       case 'portfolio':
--- src/app/details-pane/details-pane.component.ts
+++ src/app/details-pane/details-pane.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-details-pane',
   templateUrl: './details-pane.component.html',
-  styleUrls: ['./details-pane.component.css']
+  styleUrls: ['./details-pane.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DetailsPaneComponent implements OnInit {
+
+  //@Input() activePane: PaneType = 'left';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
 }
+
+//type PaneType = 'left' | 'right';
